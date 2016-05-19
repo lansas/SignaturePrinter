3f092b3d4f5df177737e417ca8d2e04dc63e93d2
sam.petrosov <sam.petrosov@gmail.com>
2016-05-19 15:21:38 +0400

    Changed printSignature error callback

--- www/SignaturePrinter.js
+++ www/SignaturePrinter.js
@@ -1,10 +1,8 @@
 window.initPrinter = function(printer, callbackSuccess,callbackError) {
     cordova.exec(callbackSuccess,callbackError, "SignaturePrinter", "initPrinter", [printer]);
 };
-window.printSignature = function(image,width,height, callback) {
-    cordova.exec(callback, function(err) {
-        console.log("Error while printSignature: "+err);
-    }, "SignaturePrinter", "printSignature", [image,width,height]);
+window.printSignature = function(image,width,height, callbackSuccess,callbackError) {
+    cordova.exec(callbackSuccess, callbackError, "SignaturePrinter", "printSignature", [image,width,height]);
 };
 window.printText = function(message, callback) {
     cordova.exec(callback, function(err) {
